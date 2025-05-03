8a699820ab3c4e25712589c3c51b152715e97079
artesgo <artesgo@gmail.com>
2025-05-03 00:43:52 -0400

    feat: emailjs contact form

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,1 +1,82 @@
-contact
+<script lang="ts">
+	import { enhance } from '$app/forms';
+	import { onMount } from 'svelte';
+	import { PUBLIC_KEE, PUBLIC_ID, PUBLIC_TEMPLATE } from '$env/static/public';
+	import { send, init } from '@emailjs/browser';
+	import { themeFacade } from '$lib';
+
+	let email = $state('');
+	let name = $state('');
+	let title = $state('');
+	let message = $state('');
+	let time = $state(Date.now());
+
+	onMount(() => {
+		setTimeout(() => {
+			time = Date.now();
+		}, 1000);
+	});
+
+	async function sendEmail() {
+		init({ publicKey: PUBLIC_KEE });
+		const emailRequest = {
+			title,
+			name,
+			email,
+			message,
+			time
+		};
+		const response = await send(PUBLIC_ID, PUBLIC_TEMPLATE, emailRequest, {
+			publicKey: PUBLIC_KEE
+		});
+	}
+</script>
+
+<form class={themeFacade().theme}>
+	<input name="time" placeholder="time" type="text" class="sr-only" bind:value={time} />
+
+	<input
+		name="email"
+		placeholder="email"
+		class="input input-primary"
+		type="text"
+		bind:value={email}
+	/>
+	<input name="name" placeholder="name" class="input input-primary" type="text" bind:value={name} />
+	<input
+		name="title"
+		placeholder="title"
+		class="input input-primary"
+		type="text"
+		bind:value={title}
+	/>
+	<textarea name="message" placeholder="message" class="input input-primary" bind:value={message}
+	></textarea>
+
+	<button class="btn btn-primary" type="button" onclick={sendEmail}>Send Message</button>
+</form>
+
+<style>
+	.dark textarea,
+	.dark input {
+		color: white;
+		background: #333;
+	}
+	.light textarea,
+	.light input {
+		color: black;
+		background: #dfdfdf;
+	}
+	input,
+	textarea {
+		display: block;
+		width: 100%;
+		color: black;
+		height: 96px;
+		margin-bottom: 12px;
+		font-size: 14px;
+	}
+	input {
+		height: 24px;
+	}
+</style>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -6,16 +6,16 @@
 <div class="flex flex-col md:flex-row gap-2 mb-4">
 	<img class="mx-auto" src={assetBaseUrl + "/unity.png"} alt="" width="228" height="293" />
 	<div>
-		[PREVIOUS CONTENT] Jana Vizdal In Unity We Soar ARCH OF PEACE COLLABORATIVE PROJECTS MISSION:
+		Jana Vizdal In Unity We Soar ARCH OF PEACE COLLABORATIVE PROJECTS MISSION:
 		FINANCIAL SUPPORT for schools in need/ portion of proceeds from these projects will be donated
 		to the participating schools. EXCELLENCE in arts and practical life application. BUILDING
 		STUDENT: CONFIDENCE, SELF EXPRESSION, CREATIVITY WHILE BUILDING SUCCESSFUL PORTFOLIOS.
 	</div>
 </div>
 
-<div class="flex flex-row gap-2 mb-4">
+<!-- <div class="flex flex-row gap-2 mb-4">
 	<img src={assetBaseUrl + "/placeholder.png"} alt="placeholder" width="228" height="40" />
 	<div>
 		Past Project missing from old site
 	</div>
-</div>
+</div> -->
